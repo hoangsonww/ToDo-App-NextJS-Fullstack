c17c40613b83906b74bb02f4dad5a5e91fbfe32d
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:16:14 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for ToDo-App-NextJS-Fullstack.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:16:13Z'
+});
